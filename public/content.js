b0949da465619641df26cac22ccfa9d011dbc8f2
Shrikant Nemiwal <shrikant.nemiwal1@gmail.com>
2023-11-04 16:13:27 +0530

    blocking website content before load

--- public/content.js
+++ public/content.js
@@ -1,24 +1,65 @@
-// const headings = document.querySelectorAll("span");
+function extractVisibleText(element) {
+  if (element.nodeType === Node.TEXT_NODE) {
+    // This node is a text node, return its text content
+    return element.textContent;
+  } else if (element.nodeType === Node.ELEMENT_NODE) {
+    if (isHidden(element)) {
+      // Skip hidden elements (e.g., display: none)
+      return "";
+    }
 
-// headings.forEach((heading) => {
-//   heading.textContent = "MODIFIED!!!!!!!!";
-// });
+    // This node is an element node, traverse its child nodes
+    let text = "";
+    for (let child of element.childNodes) {
+      text += extractVisibleText(child);
+    }
+    return text;
+  } else {
+    // Unsupported node type, return an empty string
+    return "";
+  }
+}
+
+function isHidden(element) {
+  const style = getComputedStyle(element);
+  return style.display === "none" || style.visibility === "hidden";
+}
+
+const getData = async () => {
+  try {
+    const res = "";
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   const body = document.body;
 
   if (body) {
     body.style.display = "block";
     const overlay = document.createElement("div");
-    overlay.id = "myOverlay";
-    overlay.style.position = "fixed";
-    overlay.style.top = "0";
-    overlay.style.left = "0";
-    overlay.style.width = "100%";
-    overlay.style.height = "100%";
-    overlay.style.background = "rgba(0, 0, 0, 0.7)";
-    overlay.style.zIndex = "9999";
-    overlay.style.display = "block";
+    overlay.classList.add("overlay");
     document.body.appendChild(overlay);
-    console.log("overlayed");
+
+    const overlayHTML = `
+    <div class="overlay-content">
+      <h1>Peace Web</h1>
+      <p>Searching the website ${window.location.hostname} for any profanity</p>
+      <div class="loader"></div>
+    </div>
+    `;
+    overlay.innerHTML = overlayHTML;
+    // overlay.innerHTML = "<p>Checking for any foul content...</p>";
+    // const loadingMessage = overlay.querySelector("p");
+    // loadingMessage.classList.add("overlay-content");
+
+    const rootElement = document.body;
+    const visibleText = extractVisibleText(rootElement);
+    console.log(visibleText);
+
+    // const pageHTML = document.documentElement.outerHTML;
+    // console.log(pageHTML);
   }
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const handleClick = () => {
-    console.log("click");
-  };
+  const [isOn, setIsOn] = useState("On");
+
   return (
     <>
       <h1>Peace Web</h1>
-      <div className="card">
-        <button onClick={handleClick}>Click</button>
+      <div className="container">
+        <div className="switches-container">
+          <input
+            type="radio"
+            id="switchOn"
+            name="switchPlan"
+            value="On"
+            checked={isOn === "On"}
+            onClick={() => setIsOn("On")}
+          />
+          <input
+            type="radio"
+            id="switchOff"
+            name="switchPlan"
+            value="Off"
+            checked={isOn === "Off"}
+            onClick={() => setIsOn("Off")}
+          />
+          <label htmlFor="switchOn">On</label>
+          <label htmlFor="switchOff">Off</label>
+          <div className="switch-wrapper">
+            <div className="switch">
+              <div>On</div>
+              <div>Off</div>
+            </div>
+          </div>
+        </div>
+        <p>
+          <small>NB: Input radios are used toggle the switch state.</small>
+        </p>
       </div>
     </>
   );
